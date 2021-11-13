96044789f66e33d1f6ee215af3c318f3b5aee96e
Vlada <vladamalisic78@gmail.com>
2021-11-13 17:59:52 +0100

    Create Next button

--- src/components/Bookables/BookablesList.jsx
+++ src/components/Bookables/BookablesList.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {bookables} from '../../static.json';
-
+import { FaArrowRight } from 'react-icons/fa';
 
 export default function BookablesList() {
     const group = "Rooms";
     const bookablesInGroup = bookables.filter(b => b.group === group);
-    const bookablesIndex=1;
+    const [bookableIndex, setBookableIndex] = useState(1)
+    const [selected, setSelected] = useState('Lecture Hall');
+
+    // const changeBookable = (selectedIndex) =>{
+    //     bookablesIndex = selectedIndex;
+    //     console.log(bookablesIndex)
+    // }
+    const nextBookable =() =>{
+        setBookableIndex((i=>i+1) % bookablesInGroup.length)
+    }
 
     return (
+        <div>
         <ul className="bookables items-list-nav">
             {bookablesInGroup.map((b,i) =>(
                 <li 
                     key={i}
-                    className={i === bookablesIndex ? "selected": null}
+                    className={i === bookableIndex ? "selected": null}
                 >
-                   <button className="btn"> {b.title}</button>
+                   <button className="btn" onClick={()=>setBookableIndex(i)}> {b.title}</button>
                 </li>
             ))}
-        </ul >
+        </ul>
+        <p>
+            <button className="btn" onClick={nextBookable} autoFocus>
+                <FaArrowRight />
+                <span>Next</span>
+            </button>
+        </p>
+        </div>
     )
 }
